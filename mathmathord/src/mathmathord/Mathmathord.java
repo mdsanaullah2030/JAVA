bc462051b028c38803c53e8a4b0e6dd28c2bd35c
mdsanaullah2030 <sanaullah1997215@gmail.com>
2024-04-25 16:30:21 +0600

    add

--- mathmathord/src/mathmathord/Mathmathord.java
+++ mathmathord/src/mathmathord/Mathmathord.java
@@ -10,7 +10,7 @@ public class Mathmathord {
     public static void main(String[] args) {
         Scanner Scanner=new Scanner(System.in);
         int mathord=Scanner.nextInt();
-        System.out.println(Math.max(5,20));
+        System.out.println(Math.max(5,20));       
     }
     
 }
---
+++ string/src/string/String.java
@@ -0,0 +1,16 @@
+
+package string;
+
+import java.util.Scanner;
+
+
+public class String {
+
+  
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Einter Email id");
+      String userEmail=s.nextLine();
+    }
+    
+}
