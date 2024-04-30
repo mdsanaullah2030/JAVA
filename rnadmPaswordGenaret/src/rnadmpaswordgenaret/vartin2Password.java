10c31a7df39565b6a92f54495a8aa056ac13fa54
mdsanaullah2030 <sanaullah1997215@gmail.com>
2024-04-30 16:32:59 +0600

    add

---
+++ rnadmPaswordGenaret/src/rnadmpaswordgenaret/vartin2Password.java
@@ -0,0 +1,69 @@
+
+package rnadmpaswordgenaret;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class vartin2Password {
+    public static void main(String[] args) {
+                
+
+
+
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter password length");
+        int passLength = s.nextInt();
+        
+       String password= passGenarator(passLength);
+        System.out.println("Password is:  "+password);
+
+    }
+
+    public static String passGenarator(int passLength) {
+        Random random = new Random();
+        String pass = "";
+        String finalPassword = "";
+
+        //System.out.println(range);
+        for (int i = 1; i <= passLength; i++) {
+
+            int range = random.nextInt(3) + 1;
+            switch (range) {
+                case 1:
+                    pass += (char) (random.nextInt(26) + 65);
+
+                    break;
+                case 2:
+                    pass += (char) (random.nextInt(26) + 97);
+
+                    break;
+                case 3:
+                    pass += (char) (random.nextInt(10) + 48);
+
+                    break;
+
+            }
+
+        }
+
+        for (int i = 0; i < pass.length(); i++) {
+
+            if (i == pass.length() / 2) {
+                finalPassword += "_" + pass.charAt(i);
+            } else {
+                finalPassword += pass.charAt(i); // pass=ABCD1234
+
+            }
+
+        }
+        return finalPassword;
+    }
+
+}
+
+
+    
+    
+
