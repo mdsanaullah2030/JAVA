e6536d7b1309dbf6138120ad56a693ff98ad9bad
mdsanaullah2030 <158471899+mdsanaullah2030@users.noreply.github.com>
2024-05-13 12:06:44 +0600

    13.05.2024

--- avidence/src/avidence/Avidence.java
+++ avidence/src/avidence/Avidence.java
@@ -1,28 +1,15 @@
 
 package avidence;
 
-<<<<<<< HEAD
 
+import java.util.Scanner;
 public class Avidence {
 
     
     public static void main(String[] args) {
       
-    }
     
-=======
-//import java.util.Scanner;
 
-import java.util.Scanner;
-
-
-
-public class Avidence {
-
-   
-    public static void main(String[] args) {
-        
-  
         Scanner scanner=new Scanner(System.in);
        
          int total = 0;
@@ -40,5 +27,5 @@ public class Avidence {
         System.out.println("The sum of positive integers is: " + total);
         scanner.close();
     }
->>>>>>> 31c1b8f2e70def88cb132eef621c090ec209dc4a
+
 }
